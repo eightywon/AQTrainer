1cd7c88dbcc6c5b3a027ba5581901e47b4e05de5
eightywon <scott.neitzel@gmail.com>
2017-04-21 23:37:16 -0500

    0.0.10

--- app/src/main/java/com/eightywon/aqtrainer/StageOneFragment.java
+++ app/src/main/java/com/eightywon/aqtrainer/StageOneFragment.java
@@ -1,8 +1,11 @@
 package com.eightywon.aqtrainer;
 
+import android.content.SharedPreferences;
 import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.speech.tts.TextToSpeech;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,9 +14,14 @@ import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.Switch;
 import java.io.IOException;
+import java.util.Locale;
+
+import android.os.Handler;
+
 import android.widget.Toast;
 
-public class StageOneFragment extends Fragment {
+public class StageOneFragment extends Fragment implements TextToSpeech.OnInitListener {
+
     final static int STEP_STAGE_DESCRIPTION=0;
     final static int STEP_PREP_START=1;
     final static int STEP_PREP_IN_PROGRESS=2;
@@ -25,7 +33,7 @@ public class StageOneFragment extends Fragment {
     final static int STEP_DONE=99;
 
     static String STEP_BREAK_MP3="S2.ogg";
-    static String FIRE_MP3="";
+    static String FIRE_MP3="S30.ogg";
 
     int nextStep;
     boolean playStageDesc;
@@ -42,19 +50,24 @@ public class StageOneFragment extends Fragment {
     public static MediaPlayer mpS5;
     public static MediaPlayer mpS3;
     public static MediaPlayer mpStepBreak;
+    TextToSpeech textToSpeech;
 
     public ImageButton testButton;
 
     public static boolean isPlaying;
 
+    int countDownFireStageInterval;
     Switch swPlayStageDesc;
     Switch swPlayPrep;
+
+    Handler hCountDownFireStage;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.frag_stage_1, container,
                 false);
+
 
         mpStageDescription=new MediaPlayer();
         mpPrepStart=new MediaPlayer();
@@ -76,6 +89,8 @@ public class StageOneFragment extends Fragment {
         isPlaying=false;
         playStageDesc=swPlayStageDesc.isChecked();
         playPrep=swPlayPrep.isChecked();
+
+        textToSpeech = new TextToSpeech(getActivity(),this);
 
         testButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -132,6 +147,7 @@ public class StageOneFragment extends Fragment {
                                 break;
                         }
                     }
+                    hCountDownFireStage.removeCallbacks(countDownFireStage);
                     Toast.makeText(getActivity(), String.valueOf(nextStep), Toast.LENGTH_SHORT).show();
                     testButton.setImageResource(getResources().getIdentifier("@android:drawable/ic_media_play","drawable",getActivity().getPackageName()));
                     isPlaying=false;
@@ -189,7 +205,8 @@ public class StageOneFragment extends Fragment {
             mpFireStart.setDataSource(afd5.getFileDescriptor(),afd5.getStartOffset(),afd5.getLength());
             mpFireStart.prepare();
 
-            mpFireInProgress.setDataSource(afd8.getFileDescriptor(),afd8.getStartOffset(),afd8.getLength());
+            AssetFileDescriptor afdFireInProgress=getActivity().getAssets().openFd(FIRE_MP3);
+            mpFireInProgress.setDataSource(afdFireInProgress.getFileDescriptor(),afdFireInProgress.getStartOffset(),afdFireInProgress.getLength());
             mpFireInProgress.prepare();
 
             mpFireEnd.setDataSource(afd10.getFileDescriptor(),afd10.getStartOffset(),afd10.getLength());
@@ -268,6 +285,7 @@ public class StageOneFragment extends Fragment {
         mpFireInProgress.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer p) {
+                hCountDownFireStage.removeCallbacks(countDownFireStage);
                 mpFireInProgress.pause();
                 mpFireInProgress.seekTo(0);
                 nextStep=STEP_FIRE_END;
@@ -293,6 +311,8 @@ public class StageOneFragment extends Fragment {
                 playNext(nextStep);
             }
         });
+
+        hCountDownFireStage=new Handler();
         return rootView;
     }
 
@@ -325,6 +345,9 @@ public class StageOneFragment extends Fragment {
             case STEP_FIRE_IN_PROGRESS:
                 mpFireInProgress.seekTo(0);
                 mpFireInProgress.start();
+                SharedPreferences prefs=PreferenceManager.getDefaultSharedPreferences(getContext());
+                countDownFireStageInterval=Integer.parseInt(prefs.getString("lpSettingsAnnounceInterval",""));
+                hCountDownFireStage.post(countDownFireStage);
                 break;
             case STEP_FIRE_END:
                 mpFireEnd.seekTo(0);
@@ -336,6 +359,24 @@ public class StageOneFragment extends Fragment {
         }
         if (isPlaying) {
             testButton.setImageResource(getResources().getIdentifier("@android:drawable/ic_media_stop","drawable",getActivity().getPackageName()));
+        }
+    }
+
+    private Runnable countDownFireStage = new Runnable() {
+        @Override
+        public void run() {
+            int remaining=Math.round((mpFireInProgress.getDuration()-mpFireInProgress.getCurrentPosition())/1000);
+            if (remaining>=(countDownFireStageInterval)) {
+                textToSpeech.speak(String.valueOf(remaining),TextToSpeech.QUEUE_FLUSH,null,"");
+            }
+            hCountDownFireStage.postDelayed(countDownFireStage,countDownFireStageInterval*1000);
+        }
+    };
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            textToSpeech.setLanguage(Locale.US);
         }
     }
 }
